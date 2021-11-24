5fe2e17afc14ffa4a04e1a823288671968ddc1ce
Avlasia <88412801+Avlasia@users.noreply.github.com>
2021-11-24 14:34:05 +1100

    Add files via upload

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -1,17 +1,27 @@
 
-//Display Date
+//Display Date (and other logic)
 const d = new Date();
 var weekdayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 var date = `${weekdayNames[d.getDay()]} ${d.getDate()}, ${monthNames[d.getMonth()]}`
 var todayISO=d.toISOString().slice(0,10)
+console.log(todayISO)
+var tommorowISO = new Date()
+tommorowISO.setDate(tommorowISO.getDate() + 1)
+tommorowISO = tommorowISO.toISOString().slice(0,10)
+console.log(tommorowISO)
 
 document.querySelector('#RightStuff h1').textContent = date;
 
 //Tasks
 var todolist;
 todolist = [
-    ["School", "Test this task looks right", "Various details", "Computing", "2021-11-23", "1:00", true]
+    ["School", "Test this task looks right", "Various details", "Computing", "2021-11-24", "1:00", true],
+    ["School", "Something else", "", "", "2021-11-27", "5:00", false]
+]
+
+notes = [
+    "Hello","bye"
 ]
 
 var subjects = {}
@@ -27,7 +37,7 @@ var Today = document.getElementById("tToday")
 var Tommorow = document.getElementById("tTomorrow")
 var Later = document.getElementById("tLater")
 
-function displayTask(taskNo, taskDetails, due){
+function displayTask(taskNo, taskDetails){
     if(taskDetails[4]==todayISO){
         Today.innerHTML += `<p id="task${taskNo}" class="task"><img src="Images/Unchecked.png"><span class="tHeading">Do your work</span></p>`
         task = document.getElementById('task'+taskNo);
@@ -41,19 +51,71 @@ function displayTask(taskNo, taskDetails, due){
         if(taskDetails[6]==true){ //Urg
             task.innerHTML += '<span class="tUrgent">!</span>'
         }
+        if(taskDetails[3]!==''){ //Sub
+            task.innerHTML += `<span class="tSub">${taskDetails[3]}</span>`
+        }
+
         if(taskDetails[2]!==''){ //Desc
             task.innerHTML += '<br><span class="tDesc">Do it.</span><br>'
             task.querySelector('.tDesc').textContent = taskDetails[2]
         } 
 
-    } else if(due=='Tomorrow'){
-        console.log("fail")
+    } else if(taskDetails[4]== tommorowISO){
+        Tommorow.innerHTML += `<p id="task${taskNo}" class="task"><img src="Images/Unchecked.png"><span class="tHeading">Do your work</span></p>`
+        task = document.getElementById('task'+taskNo);
+        task.querySelector('.tHeading').textContent = taskDetails[1]
+        if(taskDetails[5]!==''){
+            var current = task.innerHTML
+            task.innerHTML = `<span class="tTime">Due: ${taskDetails[5]}</span>${current}`
+        } else {
+            console.log("fail")
+        }
+        if(taskDetails[6]==true){ //Urg
+            task.innerHTML += '<span class="tUrgent">!</span>'
+        }
+        if(taskDetails[3]!==''){ //Sub
+            task.innerHTML += `<span class="tSub">${taskDetails[3]}</span>`
+        }
+
+        if(taskDetails[2]!==''){ //Desc
+            task.innerHTML += '<br><span class="tDesc">Do it.</span><br>'
+            task.querySelector('.tDesc').textContent = taskDetails[2]
+        } 
+
     } else {
-        console.log("fail")
+        Later.innerHTML += `<p id="task${taskNo}" class="task"><img src="Images/Unchecked.png"><span class="tHeading">Do your work</span></p>`
+        task = document.getElementById('task'+taskNo);
+        task.querySelector('.tHeading').textContent = taskDetails[1]
+        if(taskDetails[5]!==''){
+            var current = task.innerHTML
+            task.innerHTML = `<span class="tTime">Due: ${taskDetails[4]} ${taskDetails[5]}</span>${current}`
+        } else {
+            console.log("fail")
+        }
+        if(taskDetails[6]==true){ //Urg
+            task.innerHTML += '<span class="tUrgent">!</span>'
+        }
+        if(taskDetails[3]!==''){ //Sub
+            task.innerHTML += `<span class="tSub">${taskDetails[3]}</span>`
+        }
+
+        if(taskDetails[2]!==''){ //Desc
+            task.innerHTML += '<br><span class="tDesc">Do it.</span><br>'
+            task.querySelector('.tDesc').textContent = taskDetails[2]
+        }
     }
 } 
 
-displayTask('0',todolist[0],'Today')
+function celebrateNT(){
+    if(document.getElementsById('AllCleared').textContent == '')
+
+}
+
+displayTask('0',todolist[0])
+console.log(tommorowISO)
+displayTask('1',todolist[1])
+//displayTask('0',todolist[0],'Today')
+//displayTask('0',todolist[0],'Today')
 
 /*
                 <span class="tTime">Due: 23/11/2021 5:00</span>
